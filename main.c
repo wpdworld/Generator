74e18938af53a303cede6a419bf4296097710a27
Jordan Desktop <bastard@bastard.com>
2019-05-28 07:26:31 +0100

    Debugging and updated

--- main.c
+++ main.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 
-int main ()
-{
-   //Read Args passed by command line.
-   //Run transformation method.
-   //write to file.
-}
-
 void Transform0(const char* in_path, const char* out_path)
 {
    //Copy the word and replace the first character.
-   FILE* in_txt = fopen(in_path, 'r' );
+   FILE* in_txt = fopen(in_path, "r" );
    char buff[255];
    while(fscanf(in_txt, "%s", buff))
    {
-       if(buff)
+   		//if it's a lowercase letter
+   		if (buff[0] > 96 && buff[0] < 123)
+   		{
+   			buff[0] = buff[0] - 32;
+   			printf(buff);
+   		}
    }
 
 }
@@ -36,4 +34,12 @@ void Transform2(char* in_path, char* out_path)
 void Transform3(char* in_path, char* out_path)
 {
    //Go through each line, add all combinations of 1-6 characters to the begining and end. 
+}
+
+int main ()
+{
+   //Read Args passed by command line.
+	Transform0("test.txt", "");
+   //Run transformation method.
+   //write to file.
 }
